f259e33c5f61a5f5727424b3bcb79e1698436be9
Marc-Antoine ARNAUD <marc-antoine.arnaud@luminvent.com>
2026-08-19 21:20:17 +0200

    fix: service type custom serialization

--- src/models/identity_service_type.rs
+++ src/models/identity_service_type.rs
@@ -1,9 +1,25 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serializer};
 
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, PartialEq)]
 pub enum IdentityServiceType {
   CatalogService,
   DataService,
   IssuerService,
   Custom(String),
+}
+
+impl serde::Serialize for IdentityServiceType {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: Serializer,
+  {
+    let value = match self {
+      IdentityServiceType::CatalogService => "CatalogService".to_string(),
+      IdentityServiceType::DataService => "DataService".to_string(),
+      IdentityServiceType::IssuerService => "IssuerService".to_string(),
+      IdentityServiceType::Custom(content) => content.to_string(),
+    };
+
+    serializer.serialize_str(&value)
+  }
 }
